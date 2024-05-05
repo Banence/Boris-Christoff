39a1ceb962769517b54df84cdc649041819b6592
Banence <philip.prestigio@gmail.com>
2024-05-05 13:04:14 +0300

    Fixed responsive navbar for tablet/mobile version and also added more styling and animations for the burger icon button and sidebar

---
+++ static/scripts/navbar.js
@@ -0,0 +1,8 @@
+const burgerIconElement = document.getElementById("burger-icon");
+const navbarMobileElement = document.querySelector(".nav-mobile-container");
+const burgerSvgElement = document.getElementById("burger-svg");
+
+burgerIconElement.addEventListener('click', function () {
+    navbarMobileElement.classList.toggle("visible");
+    burgerSvgElement.classList.toggle("open");
+});

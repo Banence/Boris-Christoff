14d9f1c66d5cfef3aa55e0e4d78a4ac4001bd539
Banence <philip.prestigio@gmail.com>
2024-05-11 23:15:00 +0300

    Another small breakpoint fix for the image carousel height on different width

--- static/scripts/navbar.js
+++ static/scripts/navbar.js
@@ -1,10 +1,12 @@
 const burgerIconElement = document.getElementById("burger-icon");
 const navbarMobileElement = document.querySelector(".nav-mobile-container");
 const burgerSvgElement = document.getElementById("burger-svg");
+const crossSvgElement = document.getElementById("cross-svg");
 
 burgerIconElement.addEventListener('click', function () {
     navbarMobileElement.classList.toggle("visible");
     burgerSvgElement.classList.toggle("open");
+    crossSvgElement.classList.toggle("open");
 });
 
 

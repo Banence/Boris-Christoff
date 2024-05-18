086456c1a0c0308f697de6f53cc16d762cd76c65
Banence <philip.prestigio@gmail.com>
2024-05-19 00:34:29 +0300

    Fixed dropdown issues with mobile version of the navbar added an effect to the svg to rotate 90deg when its clicked and make the dropdown content appear. Have to fix some styling issues in the next commits about the mobile navbar

--- static/scripts/navbar.js
+++ static/scripts/navbar.js
@@ -9,4 +9,43 @@ burgerIconElement.addEventListener('click', function () {
     crossSvgElement.classList.toggle("open");
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdownSVGs = document.querySelectorAll('.dropdown-svg');
+    const dropdownItems = document.querySelectorAll('.dropdown-item-m a');
+
+    dropdownSVGs.forEach((svg, index) => {
+        svg.addEventListener('click', function(event) {
+            event.stopPropagation();
+            event.preventDefault(); 
+            const dropdown = document.querySelectorAll('.dropdown-m')[index];
+            dropdown.classList.toggle('show');
+            svg.classList.toggle('rotated');
+        });
+    });
+
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.stopPropagation();
+            event.preventDefault();
+            const navbar = document.querySelector('.nav-mobile-container');
+            navbar.classList.remove('visible');
+            window.location.href = this.href; 
+        });
+    });
+
+    document.addEventListener('click', function() {
+        const dropdowns = document.querySelectorAll('.dropdown-m');
+        dropdowns.forEach((dropdown) => {
+            dropdown.classList.remove('show');
+        });
+        const svgs = document.querySelectorAll('.dropdown-svg');
+        svgs.forEach((svg) => {
+            svg.classList.remove('rotated');
+        });
+    });
+});
+
+
+
+
 

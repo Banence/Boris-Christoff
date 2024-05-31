027458de9a4a0414c92f8e59071218878ec8ffba
Banence <philip.prestigio@gmail.com>
2024-05-31 13:09:35 +0300

    Added a check if the user is an admin to know if he should be eligable to access the admin dashboard, an admin permission is only given manually

--- app.py
+++ app.py
@@ -20,18 +20,18 @@ login_manager.login_view = "login"
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    admin = db.Column(db.Integer, nullable=False, default=0)  # Default to 0 for non-admin users
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(
         min=4, max=20)], render_kw={"placeholder": "Username"})
     
     password = PasswordField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder": "Passowrd"})
+        min=4, max=20)], render_kw={"placeholder": "Password"})  # Fixed typo
     
     submit = SubmitField("Register")
 
@@ -48,22 +48,21 @@ class LoginForm(FlaskForm):
         min=4, max=20)], render_kw={"placeholder": "Username"})
     
     password = PasswordField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder": "Passowrd"})
+        min=4, max=20)], render_kw={"placeholder": "Password"})  # Fixed typo
     
     submit = SubmitField("Login")
 
-
 @app.route('/', methods=["GET"])
 def getIndex():
-    return render_template('index.html', Title="Home"), 201
+    return render_template('index.html', Title="Home"), 200  # Changed to 200
 
 @app.route('/who-we-are', methods=["GET"])
 def getManagementAndTeam():
-    return render_template('whoWeAre.html', Title="Who We Are"), 201
+    return render_template('whoWeAre.html', Title="Who We Are"), 200  # Changed to 200
 
 @app.route('/partners', methods=["GET"])
 def getPartners():
-    return render_template('partners.html', Title="Partners"), 201
+    return render_template('partners.html', Title="Partners"), 200  # Changed to 200
 
 @app.route('/login', methods=["GET", "POST"])
 def getLogin():
@@ -71,17 +70,19 @@ def getLogin():
 
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect('/admin_dashboard')
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect('/admin_dashboard')
 
-    return render_template('login.html', form=form), 201
+    return render_template('login.html', form=form), 200  # Changed to 200
 
 @app.route('/admin_dashboard', methods=["GET", "POST"])
 @login_required
 def admin_dashboard():
-    return render_template('admin.html'), 201
+    if current_user.admin == 1:
+        return render_template('admin.html', user=current_user), 200  # Changed to 200
+    else:
+        return render_template('/includes/404.html'), 404
 
 @app.route('/logout', methods=["GET", "POST"])
 @login_required
@@ -94,13 +95,15 @@ def getRegister():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(username=form.username.data, password=hashed_password, admin=0)  # Default admin to 0
         db.session.add(new_user)
         db.session.commit()
         return redirect('/login')
 
-    return render_template('register.html', form=form), 201
+    return render_template('register.html', form=form), 200  # Changed to 200
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)

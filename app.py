8c09dd093500cdbf457e98bafb0e2f1fd53dc34d
Banence <philip.prestigio@gmail.com>
2024-06-04 20:31:03 +0300

    Made the admin dashboard accessable only if the user is admin and also added a upload-partner page where admins can upload partners using for, update them(about to implemente that later) and delete them. After they have been added they should be displayed as a grid in the partners.html page. And for last fixed some minor issues about the styling and animations

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import FileField, StringField, PasswordField, SubmitField
 from werkzeug.utils import secure_filename
 import os
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms.validators import InputRequired, Length, ValidationError, URL
 from flask_bcrypt import Bcrypt
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['UPLOAD_FOLDER'] = 'static/files/uploaded_logos'
 app.config['SECRET_KEY'] = 'admin1234'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -29,11 +29,24 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-    admin = db.Column(db.Integer, nullable=False, default=0)  # Default to 0 for non-admin users
+    admin = db.Column(db.Integer, nullable=False, default=0)
 
-class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()], render_kw={"class": "file-input"})
-    submit = SubmitField("Upload File", render_kw={"class": "btn"})
+class Partners(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False, unique=True)
+    website = db.Column(db.String(40), nullable=False, unique=True)
+    logo = db.Column(db.String(120), nullable=False)
+
+class UploadPartnerForm(FlaskForm):
+    logo = FileField("Logo", validators=[InputRequired()], render_kw={"class": "file-input"})
+    name = StringField("Name", validators=[InputRequired(), Length(max=40)], render_kw={"class": "input-primary", "placeholder": "Partner Name"})
+    website = StringField("Website", validators=[InputRequired(), URL(), Length(max=40)], render_kw={"class": "input-primary", "placeholder": "Partner Website"})
+    submit = SubmitField("Add Partner", render_kw={"class": "btn"})
+
+    def validate_name(self, name):
+        existing_partner = Partners.query.filter_by(name=name.data).first()
+        if existing_partner:
+            raise ValidationError("That partner name already exists. Please choose a different one.")
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username", "class": "input-primary"})
@@ -58,11 +71,7 @@ def getIndex():
 def getManagementAndTeam():
     return render_template('whoWeAre.html', Title="Who We Are")
 
-@app.route('/partners', methods=["GET"])
-def getPartners():
-    return render_template('partners.html', Title="Partners")
-
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/admin/login', methods=["GET", "POST"])
 def getLogin():
     form = LoginForm()
     if form.validate_on_submit():
@@ -70,42 +79,77 @@ def getLogin():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
             return redirect(url_for('admin_dashboard'))
-    return render_template('login.html', form=form, Title="Admin Login")
+    return render_template('admin/login.html', form=form, Title="Admin Login")
 
-@app.route('/admin_dashboard', methods=["GET", "POST"])
-@login_required
+@app.route('/admin/admin_dashboard', methods=["GET", "POST"])
 def admin_dashboard():
-    if current_user.admin == 1:
-        form = UploadFileForm()
-        if form.validate_on_submit():
-            file = form.file.data
-            
-            # Ensure the upload folder exists
-            if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                os.makedirs(app.config['UPLOAD_FOLDER'])
-            
-            # Save the file
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-        return render_template('admin.html', user=current_user, form=form, Title="Admin Dashboard")
-    else:
+    try:
+        if current_user.admin == 1:
+            return render_template('admin/admin.html', user=current_user, Title="Admin Dashboard")
+        else:
+            return render_template('includes/404.html'), 404
+    except:
         return render_template('includes/404.html'), 404
 
-@app.route('/logout', methods=["GET", "POST"])
+@app.route('/admin/logout', methods=["GET", "POST"])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('getLogin'))
 
-@app.route('/register', methods=["GET", "POST"])
+@app.route('/admin/register', methods=["GET", "POST"])
 def getRegister():
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        new_user = User(username=form.username.data, password=hashed_password, admin=0)  # Default admin to 0
+        new_user = User(username=form.username.data, password=hashed_password, admin=0) 
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('getLogin'))
-    return render_template('register.html', form=form, Title="Admin Register")
+    return render_template('admin/register.html', form=form, Title="Admin Register")
+
+@app.route('/admin/upload-partner', methods=["GET", "POST"])
+def get_admin_partner_upload():
+    form = UploadPartnerForm()
+    partners = Partners.query.all()
+    try:
+        if current_user.admin == 1:
+            if form.validate_on_submit():
+                logo = form.logo.data
+                logo_filename = secure_filename(logo.filename)
+                logo_path = os.path.join(app.config['UPLOAD_FOLDER'], logo_filename)
+                logo.save(logo_path)
+
+                new_partner = Partners(
+                    name=form.name.data,
+                    website=form.website.data,
+                    logo=logo_filename
+                )
+                db.session.add(new_partner)
+                db.session.commit()
+                flash('Partner added successfully!', 'success')
+            return render_template('admin/upload-partner.html', form=form, Title="Upload Partner", partners=partners), 200
+        else:
+            return render_template('includes/404.html'), 404
+    except:
+        return render_template('includes/404.html'), 404
+    
+@app.route('/partners', methods=["GET"])
+def getPartners():
+    partners = Partners.query.all()
+    return render_template('partners.html', Title="Partners", partners=partners)
+
+@app.route('/admin/upload-partners/del/<int:id>', methods=["POST"])
+def deletePartner(id):
+    partner_id = id
+    if current_user.admin == 1:
+        partner = Partners.query.get(partner_id)
+        db.session.delete(partner)
+        db.session.commit()
+        return redirect(url_for('admin_dashboard'))
+    else:
+        return render_template('includes/404.html'), 404
+
 
 if __name__ == '__main__':
     with app.app_context():

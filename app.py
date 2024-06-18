122397a3635ff13b390f278cf1df2f9f8127e131
Banence <philip.prestigio@gmail.com>
2024-06-19 01:57:45 +0300

    Added the logic for updating, uploading, deleting events like for partners. Also added styling for that. When the event is uploaded it is stored in the database. If the event has to be deleted it is done using DELETE, PUT or PATCH using ajax and js

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import FileField, StringField, PasswordField, SubmitField, HiddenField
+from wtforms import FileField, StringField, PasswordField, SubmitField, HiddenField, DateField, TextAreaField
 from werkzeug.utils import secure_filename
 import os
-from wtforms.validators import InputRequired, Length, ValidationError, URL
+from wtforms.validators import InputRequired, Length, ValidationError, URL, DataRequired
 from flask_bcrypt import Bcrypt
+from flask_wtf.file import FileAllowed
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['UPLOAD_FOLDER'] = 'static/files/uploaded_logos'
+app.config['UPLOAD_FOLDER'] = 'static/files/uploaded_pictures'
 app.config['SECRET_KEY'] = 'admin1234'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -36,6 +37,13 @@ class Partners(db.Model):
     name = db.Column(db.String(40), nullable=False, unique=True)
     website = db.Column(db.String(40), nullable=False, unique=True)
     logo = db.Column(db.String(120), nullable=False)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    summary = db.Column(db.Text, nullable=False)
+    picture = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
 
 class UploadPartnerForm(FlaskForm):
     partner_id = HiddenField()
@@ -64,6 +72,13 @@ class LoginForm(FlaskForm):
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password", "class": "input-primary"})
     submit = SubmitField("Login", render_kw={"class": "btn"})
 
+class EventForm(FlaskForm):
+    name = StringField('Event Name', validators=[DataRequired()], render_kw={"class": "input-primary"})
+    summary = TextAreaField('Summary', validators=[DataRequired()], render_kw={"class": "textarea-primary"})
+    picture = FileField('Picture', render_kw={"class": "file-input"})
+    description = TextAreaField('Description', validators=[DataRequired()], render_kw={"class": "textarea-primary"})
+    submit = SubmitField('Upload Event', render_kw={"class": "btn", "style": "background-color: #f5494c;"})
+
 @app.route('/', methods=["GET"])
 def getIndex():
     return render_template('index.html', Title="Home")
@@ -80,17 +95,15 @@ def getLogin():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
             return redirect(url_for('admin_dashboard'))
+        flash('Invalid username or password', 'error')
     return render_template('admin/login.html', form=form, Title="Admin Login")
 
 @app.route('/admin/admin_dashboard', methods=["GET", "POST"])
+@login_required
 def admin_dashboard():
-    try:
-        if current_user.admin == 1:
-            return render_template('admin/admin.html', user=current_user, Title="Admin Dashboard")
-        else:
-            return render_template('includes/404.html'), 404
-    except:
-        return render_template('includes/404.html'), 404
+    if current_user.admin == 1:
+        return render_template('admin/admin.html', user=current_user, Title="Admin Dashboard")
+    return render_template('includes/404.html'), 404
 
 @app.route('/admin/logout', methods=["GET", "POST"])
 @login_required
@@ -106,87 +119,138 @@ def getRegister():
         new_user = User(username=form.username.data, password=hashed_password, admin=0) 
         db.session.add(new_user)
         db.session.commit()
+        flash('User registered successfully!', 'success')
         return redirect(url_for('getLogin'))
     return render_template('admin/register.html', form=form, Title="Admin Register")
 
 @app.route('/admin/upload-partner', methods=["GET", "POST"])
+@login_required
 def get_admin_partner_upload():
     form = UploadPartnerForm()
     partners = Partners.query.all()
-    try:
-        if current_user.admin == 1:
-            if form.validate_on_submit():
-                logo = form.logo.data
-                logo_filename = secure_filename(logo.filename)
-                logo_path = os.path.join(app.config['UPLOAD_FOLDER'], logo_filename)
-                logo.save(logo_path)
+    if current_user.admin == 1:
+        if form.validate_on_submit():
+            logo = form.logo.data
+            logo_filename = secure_filename(logo.filename)
+            logo_path = os.path.join(app.config['UPLOAD_FOLDER'], logo_filename)
+            logo.save(logo_path)
 
-                new_partner = Partners(
-                    name=form.name.data,
-                    website=form.website.data,
-                    logo=logo_filename
-                )
-                db.session.add(new_partner)
-                db.session.commit()
-                flash('Partner added successfully!', 'success')
-                return redirect('admin_dashboard')
-            return render_template('admin/upload-partner.html', form=form, Title="Upload Partner", partners=partners), 200
-        else:
-            return render_template('includes/404.html'), 404
-    except:
-        return render_template('includes/404.html'), 404
-    
+            new_partner = Partners(
+                name=form.name.data,
+                website=form.website.data,
+                logo=logo_filename
+            )
+            db.session.add(new_partner)
+            db.session.commit()
+            flash('Partner added successfully!', 'success')
+            return redirect(url_for('admin_dashboard'))
+        return render_template('admin/upload-partner.html', form=form, Title="Upload Partner", partners=partners)
+    return render_template('includes/404.html'), 404
+
 @app.route('/partners', methods=["GET"])
 def getPartners():
     partners = Partners.query.all()
     return render_template('partners.html', Title="Partners", partners=partners)
 
-@app.route('/admin/upload-partners/del/<int:id>', methods=["DELETE"])
-def deletePartner(id):
-    partner_id = id
-    try:
-        if current_user.admin == 1:
-            partner = Partners.query.get(partner_id)
-            db.session.delete(partner)
-            db.session.commit()
-            return '', 204 
-        else:
-            return render_template('includes/404.html'), 404
-    except:
-        return render_template('includes/404.html'), 404
-
-    
-@app.route('/admin/update-partner/<int:partner_id>', methods=["GET", "PUT", "PATCH"])
+@app.route('/admin/update-partner/<int:partner_id>', methods=["GET", "POST"])
+@login_required
 def update_partner(partner_id):
     partner = Partners.query.get_or_404(partner_id)
     form = UploadPartnerForm(partner_id=partner.id)
-    try:
-        if current_user.admin == 1:
-            if request.method in ["PUT", "PATCH"]:
-                if form.validate_on_submit():
-                    partner.name = form.name.data
-                    partner.website = form.website.data
+    if current_user.admin == 1:
+        if request.method in ["POST", "PATCH"]:
+            if form.validate_on_submit():
+                partner.name = form.name.data
+                partner.website = form.website.data
 
-                    if form.logo.data:
-                        logo = form.logo.data
-                        logo_filename = secure_filename(logo.filename)
-                        logo_path = os.path.join(app.config['UPLOAD_FOLDER'], logo_filename)
-                        logo.save(logo_path)
-                        partner.logo = logo_filename
+                if form.logo.data:
+                    logo = form.logo.data
+                    logo_filename = secure_filename(logo.filename)
+                    logo_path = os.path.join(app.config['UPLOAD_FOLDER'], logo_filename)
+                    logo.save(logo_path)
+                    partner.logo = logo_filename
 
-                    db.session.commit()
-                    return '', 204  
+                db.session.commit()
+                flash('Partner updated successfully!', 'success')
+                return redirect(url_for('admin_dashboard'))
+            else:
+                flash_errors(form)
+        elif request.method == 'GET':
+            form.name.data = partner.name
+            form.website.data = partner.website
 
-            elif request.method == 'GET':
-                form.name.data = partner.name
-                form.website.data = partner.website
+        return render_template('admin/update-partner.html', form=form, Title="Update Partner", partner=partner)
+    return render_template('includes/404.html'), 404
 
-            return render_template('admin/update-partner.html', form=form, Title="Update Partner", partner=partner)
+@app.route("/events", methods=["GET"])
+def getEvents():
+    events = Event.query.all()
+    return render_template("events.html", Title="Events", events=events)
+
+@app.route('/admin/upload-event', methods=['GET', 'POST'])
+@login_required
+def upload_event():
+    form = EventForm()
+    if form.validate_on_submit():
+        picture = form.picture.data
+        if picture:
+            picture_filename = secure_filename(picture.filename)
+            picture_path = os.path.join(app.config['UPLOAD_FOLDER'], picture_filename)
+            picture.save(picture_path)
+
+            new_event = Event(
+                name=form.name.data,
+                summary=form.summary.data,
+                picture=picture_filename,
+                description=form.description.data
+            )
+            db.session.add(new_event)
+            db.session.commit()
+            flash('Event uploaded successfully!', 'success')
+            return redirect(url_for('upload_event'))
+
+    events = Event.query.all()
+    return render_template('admin/upload-event.html', form=form, events=events)
+
+
+@app.route('/admin/update-event/<int:event_id>', methods=['GET', 'POST', 'PUT'])
+@login_required
+def update_event(event_id):
+    event = Event.query.get_or_404(event_id)
+    form = EventForm(obj=event)
+
+    if request.method in ['POST', 'PUT']:
+        if form.validate_on_submit():
+            event.name = form.name.data
+            event.summary = form.summary.data
+            event.description = form.description.data
+
+            if form.picture.data:
+                picture = form.picture.data
+                picture_filename = secure_filename(picture.filename)
+                picture_path = os.path.join(app.config['UPLOAD_FOLDER'], picture_filename)
+                picture.save(picture_path)
+                event.picture = picture_filename
+
+            db.session.commit()
+            flash('Event updated successfully!', 'success')
+            return redirect(url_for('getEvents'))
         else:
-            return render_template('includes/404.html'), 404
-    except:
-        return render_template('includes/404.html'), 404
+            flash_errors(form)
 
+    return render_template('admin/update-event.html', form=form, event=event)
+
+@app.route('/admin/delete-event/<int:event_id>', methods=['DELETE'])
+def delete_event(event_id):
+    event = Event.query.get_or_404(event_id)
+    db.session.delete(event)
+    db.session.commit()
+    return '', 204
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(f"Error in the {getattr(form, field).label.text}: {error}", 'error')
 
 if __name__ == '__main__':
     with app.app_context():
